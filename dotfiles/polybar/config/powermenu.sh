693f4e3de388d9af0ed8857d9126cffa0c3bdc03
Gaurav Manek <gaurav@gauravmanek.com>
2017-08-30 09:42:13 -0400

    Added sleep to power menu.

--- dotfiles/polybar/config/powermenu.sh
+++ dotfiles/polybar/config/powermenu.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-rv=$(echo -e "Cancel|Lock|Log Off|Shutdown|Reboot" | rofi -dmenu -sep '|' -lines 5 -width 100 -padding 900 -mesg "Power" -u 3-4 -selected-row 1 -i -p "")
+rv=$(echo -e "Cancel|Lock|Sleep|Log Off|Shutdown|Reboot" | rofi -dmenu -sep '|' -lines 5 -width 100 -padding 900 -mesg "Power" -u 3-4 -selected-row 2 -i -p "")
 echo $rv
 
 case $rv in
    "Lock")
       (sleep .5; i3lock-wrapper) &
+      ;;
+   "Sleep")
+      (sleep .5; i3lock-wrapper) &
+      (sleep 2.5; sudo systemctl suspend) &
       ;;
    "Log Off")
       exec i3-msg exit &
