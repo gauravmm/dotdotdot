33dd882ccbc4a86cc305d6a736d5b95eed70050f
Gaurav Manek <gaurav@gauravmanek.com>
2019-02-08 11:09:30 -0500

    Support for multi-monitor polybar config.

--- dotfiles/polybar/config/launch.sh
+++ dotfiles/polybar/config/launch.sh
@@ -6,6 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar top &
-polybar bottom &
+HOST=$(hostname)
+while IFS="" read -r item || [ -n "$item" ]; do
+    export POLYBAR_DISPLAY=${item%%:*}
+    export POLYBAR_DPI=96
+    if [ "$HOST" = "regretful-arch" ] && [ "$item" = "eDP1: 1920x1080+0+0" ]; then
+        export POLYBAR_DPI=120
+    fi
+
+    echo "Setting $HOST::$item to $POLYBAR_DPI"
+
+    polybar top &
+    polybar bottom &
+done <<< $(polybar -m)
