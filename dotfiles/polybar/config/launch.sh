a78d9ca3e85cebe5f90401248c57840d300ae7a4
Gaurav Manek <gaurav@gauravmanek.com>
2019-02-08 22:56:21 -0500

    Added ingvaeonic entry to polybar

--- dotfiles/polybar/config/launch.sh
+++ dotfiles/polybar/config/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -12,6 +12,8 @@ while IFS="" read -r item || [ -n "$item" ]; do
     export POLYBAR_DPI=96
     if [ "$HOST" = "regretful-arch" ] && [ "$item" = "eDP1: 1920x1080+0+0" ]; then
         export POLYBAR_DPI=120
+    elif [ "$HOST" = "ingvaeonic" ] && [ "$item" = "DP-2: 3840x2160+0+0" ]; then
+        export POLYBAR_DPI=144
     fi
 
     echo "Setting $HOST::$item to $POLYBAR_DPI"
