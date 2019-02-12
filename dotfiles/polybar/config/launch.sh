31059819468f3470ef174775f32b095a1ad4cd39
Gaurav Manek <gaurav@gauravmanek.com>
2019-02-12 10:23:49 -0500

    More general polybar.

--- dotfiles/polybar/config/launch.sh
+++ dotfiles/polybar/config/launch.sh
@@ -10,11 +10,11 @@ HOST=$(hostname)
 while IFS="" read -r item || [ -n "$item" ]; do
     export POLYBAR_DISPLAY=${item%%:*}
     export POLYBAR_DPI=96
-    if [ "$HOST" = "regretful-arch" ] && [ "$item" = "eDP1: 1920x1080+0+0" ]; then
+    if [ "$HOST" = "regretful-arch" ] && [ "${item:0:15}" = "eDP1: 1920x1080" ]; then
         export POLYBAR_DPI=96
     elif [ "$HOST" = "regretful-arch" ] && [ "${item:0:16}" = "HDMI1: 3840x2160" ]; then
         export POLYBAR_DPI=120
-    elif [ "$HOST" = "ingvaeonic" ] && [ "$item" = "DP-2: 3840x2160+0+0" ]; then
+    elif [ "$HOST" = "ingvaeonic" ] && [ "${item:0:15}" = "DP-2: 3840x2160" ]; then
         export POLYBAR_DPI=120
     fi
 
