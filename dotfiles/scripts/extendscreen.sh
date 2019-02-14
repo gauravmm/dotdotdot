67909b43d0c3c3e410df62278dfac35a4ccda280
Gaurav Manek <gaurav@gauravmanek.com>
2019-02-13 20:31:23 -0500

    Changing screen now restarts polybar.

--- dotfiles/scripts/extendscreen.sh
+++ dotfiles/scripts/extendscreen.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
+POLYBAR_RELAUNCH="$HOME/.config/polybar/launch.sh"
+
 if [ "$HOSTNAME" = "regretful-arch" ]; then
 	rv=$(printf "PC screen only|Extend|Second screen only" | rofi -dmenu -sep '|' -lines 3 -width 100 -padding 860 -mesg "Display" -u 3-4 -selected-row 1 -i -p "")
 	echo $rv
 
 	case $rv in
-	   "PC screen only")
-	      (xrandr --output eDP1 --auto --output HDMI1 --off) &
-	      ;;
-	   "Extend")
-	      (xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1) &
-	      ;;
-	   "Second screen only")
-	      (xrandr --output eDP1 --off --output HDMI1 --auto --left-of eDP1) &
-	      ;;
+		"PC screen only")
+			xrandr --output eDP1 --auto --output HDMI1 --off
+			;;
+		"Extend")
+			xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
+			;;
+		"Second screen only")
+			xrandr --output eDP1 --off --output HDMI1 --auto
+			;;
 	esac
+
+	if [ -x "$POLYBAR_RELAUNCH" ]; then
+		"$POLYBAR_RELAUNCH"
+	fi
 fi
