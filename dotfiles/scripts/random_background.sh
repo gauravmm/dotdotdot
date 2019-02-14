eebb839add5737eed0f79a4e0c507b3a91580bd6
Gaurav Manek <gaurav@gauravmanek.com>
2019-02-14 18:33:49 -0500

    Removed fehbg

--- dotfiles/scripts/random_background.sh
+++ dotfiles/scripts/random_background.sh
@@ -6,13 +6,17 @@ if [[ ! -d "$WALLPAPER_DIR" ]]; then
     mkdir "$WALLPAPER_DIR"
 fi
 
+if [[ -f "$HOME/.fehbg" ]]; then
+    rm "$HOME/.fehbg"
+fi
+
 WALLPAPER=$(ls "$WALLPAPER_DIR" | sort -R | head -n1 | tr -s "\n")
 
 if [[ ! -e $WALLPAPER ]]; then
     echo "Setting wallpaper to $WALLPAPER_DIR/$WALLPAPER"
 
     WALLPAPER_PATH="$WALLPAPER_DIR/$WALLPAPER"
-    feh --bg-fill "$WALLPAPER_PATH"
+    feh --no-fehbg --bg-fill "$WALLPAPER_PATH"
     echo "$WALLPAPER_PATH" > "$HOME/.wallpaper"
 else
     echo "No wallpaper found."
