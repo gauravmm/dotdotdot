9f3b19d953f80b110945b5677800497635bae055
Gaurav Manek <gaurav@gauravmanek.com>
2023-10-30 10:54:57 +0700

    Added extended screen mode for new computer.

--- dotfiles/scripts/extendscreen.sh
+++ dotfiles/scripts/extendscreen.sh
@@ -4,41 +4,24 @@ set -x
 POLYBAR_RELAUNCH="$HOME/.config/polybar/launch.sh"
 WALLPAPER="$HOME/.wallpaper"
 
-if [ $(hostname) = "anartes" ]; then
+if [ $(hostname) = "alessi" ]; then
 	rv=$(printf "PC screen only|Extend|Second screen only" | rofi -dmenu -sep '|' -lines 3 -width 100 -padding 860 -mesg "Display" -u 3-4 -selected-row 1 -i -p "")
 	echo $rv
 
 	case $rv in
 		"PC screen only")
-			xrandr --output eDP1 --auto --output HDMI1 --off
+			xrandr --output eDP-1 --auto --output HDMI-1 --off
 			;;
 		"Extend")
-			xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
+			xrandr --output eDP-1 --auto --output HDMI-1 --auto --right-of eDP-1
 			;;
 		"Second screen only")
-			xrandr --output eDP1 --off --output HDMI1 --auto
-			;;
-	esac
-
-elif [ $(hostname) = "ingvaeonic" ]; then
-	rv=$(printf "Main only|Both|Secondary only" | rofi -dmenu -sep '|' -lines 3 -width 100 -padding 860 -mesg "Display" -u 3-4 -selected-row 1 -i -p "")
-	echo $rv
-
-	case $rv in
-		"Main only")
-			xrandr --output DP-2 --auto --output DP-0 --off
-			;;
-		"Both")
-			xrandr \
-				--output DP-0 --auto --panning 3840x2160+0+0 \
-				--output DP-2 --auto --panning 3440x1440+200+2160
-			;;
-		"Secondary only")
-			xrandr --output DP-2 --off --output DP-0 --auto
+			xrandr --output eDP-1 --off --output HDMI-1 --auto
 			;;
 	esac
 
 else
+	notify-send "No extend-screen script for $(hostname)."
 	exit;
 fi
 
